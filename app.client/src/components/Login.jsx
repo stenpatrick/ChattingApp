1071acf4790b1d4539bec9f6b3b10a29a1b2d780
Opilane TTHK <opilane@tthk.ee>
2024-09-13 14:31:43 +0300

    recaptcha now working

--- app.client/src/components/Login.jsx
+++ app.client/src/components/Login.jsx
@@ -1,16 +1,79 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 function Login() {
+    const [captchaToken, setCaptchaToken] = useState(null);
+    const recaptchaRef = useRef(null);
 
     document.title = "Login";
 
-    // dont ask an already logged in user to login over and over again
+    // Don't ask an already logged-in user to login over and over again
     useEffect(() => {
         const user = localStorage.getItem("user");
         if (user) {
             document.location = "/";
         }
     }, []);
+
+    const onCaptchaChange = (token) => {
+        setCaptchaToken(token);
+    };
+
+    const loginHandler = async (e) => {
+        e.preventDefault();
+        const form_ = e.target, submitter = document.querySelector("input.login");
+
+        const formData = new FormData(form_, submitter);
+        const dataToSend = {};
+
+        for (const [key, value] of formData.entries()) {
+            dataToSend[key] = value;
+        }
+
+        if (dataToSend.Remember === "on") {
+            dataToSend.Remember = true;
+        }
+
+        // Check if reCAPTCHA was completed
+        if (!captchaToken) {
+            const messageEl = document.querySelector(".message");
+            messageEl.innerHTML = "Please complete the CAPTCHA";
+            return;
+        }
+
+        dataToSend["g-recaptcha-response"] = captchaToken;
+
+        console.log("login data before send: ", dataToSend);
+        const response = await fetch("api/app/login", {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(dataToSend),
+            headers: {
+                "Content-Type": "Application/json",
+                "Accept": "application/json"
+            }
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem("user", dataToSend.Email);
+            document.location = "/";
+        }
+
+        const messageEl = document.querySelector(".message");
+        if (data.message) {
+            messageEl.innerHTML = data.message;
+        } else {
+            messageEl.innerHTML = "Something went wrong, please try again";
+        }
+
+        console.log("login error: ", data);
+
+        // Reset reCAPTCHA
+        recaptchaRef.current.reset();
+        setCaptchaToken(null);
+    };
 
     return (
         <section className='login-page-wrapper page'>
@@ -33,6 +96,12 @@ function Login() {
                         <label htmlFor="remember">Remember Password?</label>
                         <br />
                         <br />
+                        <ReCAPTCHA
+                            ref={recaptchaRef}
+                            sitekey={process.env.REACT_APP_GOOGLE_API_KEY}
+                            onChange={onCaptchaChange}
+                        />
+                        <br />
                         <input type="submit" value="Login" className='login btn' />
                     </form>
                 </div>
@@ -43,47 +112,6 @@ function Login() {
             </div>
         </section>
     );
-    async function loginHandler(e) {
-        e.preventDefault();
-        const form_ = e.target, submitter = document.querySelector("input.login");
-
-        const formData = new FormData(form_, submitter), dataToSend = {};
-
-        for (const [key, value] of formData) {
-            dataToSend[key] = value;
-        }
-
-        if (dataToSend.Remember === "on") {
-            dataToSend.Remember = true;
-        }
-
-        console.log("login data before send: ", dataToSend);
-        const response = await fetch("api/app/login", {
-            method: "POST",
-            credentials: "include",
-            body: JSON.stringify(dataToSend),
-            headers: {
-                "content-type": "Application/json",
-                "Accept": "application/json"
-            }
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            localStorage.setItem("user", dataToSend.Email);
-            document.location = "/";
-        }
-
-        const messageEl = document.querySelector(".message");
-        if (data.message) {
-            messageEl.innerHTML = data.message;
-        } else {
-            messageEl.innerHTML = "Something went wrong, please try again";
-        }
-
-        console.log("login error: ", data);
-    }
 }
 
 export default Login;
--- app.client/src/components/Register.jsx
+++ app.client/src/components/Register.jsx
@@ -1,16 +1,92 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 function Register() {
+    const [captchaToken, setCaptchaToken] = useState(null);
+    const recaptchaRef = useRef(null);
 
     document.title = "Register";
 
-    // dont ask an already registered user to register over and over again
+    // Don't ask an already registered user to register over and over again
     useEffect(() => {
         const user = localStorage.getItem("user");
         if (user) {
             document.location = "/";
         }
     }, []);
+
+    const onCaptchaChange = (token) => {
+        setCaptchaToken(token);
+    };
+
+    const registerHandler = async (e) => {
+        e.preventDefault();
+        const form_ = e.target;
+        const formData = new FormData(form_);
+        const dataToSend = {};
+
+        for (const [key, value] of formData.entries()) {
+            dataToSend[key] = value;
+        }
+
+        // Create username
+        const newUserName = dataToSend.Name.trim().split(" ");
+        dataToSend.UserName = newUserName.join("");
+
+        // Check if reCAPTCHA was completed
+        if (!captchaToken) {
+            const messageEl = document.querySelector(".message");
+            messageEl.innerHTML = "Please complete the CAPTCHA";
+            return;
+        }
+
+        dataToSend["g-recaptcha-response"] = captchaToken;
+
+        const response = await fetch("api/app/register", {
+            method: "POST",
+            credentials: "include",
+            body: JSON.stringify(dataToSend),
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            document.location = "/login";
+        }
+
+        const messageEl = document.querySelector(".message");
+        if (data.message) {
+            messageEl.innerHTML = data.message;
+        } else {
+            let errorMessages = "<div>Attention please:</div><div class='normal'>";
+            data.errors.forEach(error => {
+                errorMessages += error.description + " "
+            });
+            errorMessages += "</div>";
+            errorMessages += "<div class='password-strength-warning'>" +
+                "<p>Your password is too weak. Consider using a stronger password.</p>" +
+                "<a href='https://neal.fun/password-game/' target='_blank' rel='noopener noreferrer'>Check out this password strength game for tips!</a>" +
+                "</div>";
+            messageEl.innerHTML = errorMessages;
+        }
+
+        // Password strength recommendation message
+        const passwordInput = form_.querySelector('#password');
+        if (passwordInput) {
+            const result = zxcvbn(passwordInput.value);
+            if (result.score < 3) {
+            }
+        }
+        console.log("register error: ", data);
+
+        // Reset reCAPTCHA
+        recaptchaRef.current.reset();
+        setCaptchaToken(null);
+    };
 
     return (
         <section className='register-page-wrapper page'>
@@ -32,7 +108,12 @@ function Register() {
                         <label htmlFor="password">Password</label>
                         <br />
                         <input type="password" name='PasswordHash' id='password' required />
-
+                        <br />
+                        <ReCAPTCHA
+                            ref={recaptchaRef}
+                            sitekey={process.env.REACT_APP_GOOGLE_API_KEY}
+                            onChange={onCaptchaChange}
+                        />
                         <br />
                         <input type="submit" value="Register" className='register btn' />
                     </form>
@@ -44,51 +125,6 @@ function Register() {
             </div>
         </section>
     );
-    async function registerHandler(e) {
-        e.preventDefault();
-        const form_ = e.target, submitter = document.querySelector("input.login");
-
-        const formData = new FormData(form_, submitter), dataToSend = {};
-
-        for (const [key, value] of formData) {
-            dataToSend[key] = value;
-        }
-
-        // create username
-        const newUserName = dataToSend.Name.trim().split(" ");
-        dataToSend.UserName = newUserName.join("");
-
-        const response = await fetch("api/app/register", {
-            method: "POST",
-            credentials: "include",
-            body: JSON.stringify(dataToSend),
-            headers: {
-                "content-type": "Application/json",
-                "Accept": "application/json"
-            }
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            document.location = "/login";
-        }
-
-        const messageEl = document.querySelector(".message");
-        if (data.message) {
-            messageEl.innerHTML = data.message;
-        } else {
-            let errorMessages = "<div>Attention please:</div><div class='normal'>";
-            data.errors.forEach(error => {
-                errorMessages += error.description + " ";
-            });
-
-            errorMessages += "</div>";
-            messageEl.innerHTML = errorMessages;
-        }
-
-        console.log("login error: ", data);
-    }
 }
 
 export default Register;
